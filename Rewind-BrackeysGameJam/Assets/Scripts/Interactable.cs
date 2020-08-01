d8e8e83b1840ec6f2ab2a5a53f0224f998959297
Heather <35993724+HeatherCochrane@users.noreply.github.com>
2020-08-01 11:50:29 +0100

    Commit

---
+++ Rewind-BrackeysGameJam/Assets/Scripts/Interactable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void rewindTime()
+    {
+        //Here take the current game object and change it into the old version, a grown plant or an undestroyed building etc.
+
+
+
+    }
+}
---
+++ Rewind-BrackeysGameJam/Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+//References:
+//https://github.com/jiankaiwang/FirstPersonController
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private float translation;
+    private float straffe;
+
+    bool canHide = false;
+    bool isHiding = false;
+
+    [SerializeField]
+    Rigidbody rb;
+    bool isFalling = false;
+    [SerializeField]
+    Vector3 force;
+    void Start()
+    {
+        // turn off the cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input.GetAxis() is used to get the user's input
+        // You can furthor set it on Unity. (Edit, Project Settings, Input)
+        translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        straffe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            // turn on the cursor
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && !isFalling)
+        {
+            isFalling = true;
+            rb.AddForce(force, ForceMode.Impulse);
+        }
+
+        //Let the player hide when in a safe spot, unhide when left shift is toggled
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            if (canHide && !isHiding)
+            {
+                isHiding = true;
+            }
+            else if(isHiding)
+            {
+                isHiding = false;
+            }
+        }
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Hide")
+        {
+            canHide = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Hide")
+        {
+            canHide = false;
+            isHiding = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Ground")
+        {
+            isFalling = false;
+        }
+    }
+}
